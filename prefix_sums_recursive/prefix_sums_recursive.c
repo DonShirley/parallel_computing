4b0633a8a9aaca80b9fdaa8fed8b8a2cdb2d4f29
adrian <adriankempf@mail.ua>
2013-12-28 19:13:25 +0100

    Wenn man die Folien sich doch einmal ansieht, findet man den kompletten Code für alle Beispiele in den Folien :P. Also gibt es jetzt auch ein gescheites rekursives Programm.

--- prefix_sums_recursive/prefix_sums_recursive.c
+++ prefix_sums_recursive/prefix_sums_recursive.c
@@ -12,22 +12,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-int argcounter;
+//int argcounter;
+//
+//void prefix_sums(register int sum, char **argv)
+//{
+//	static int counter = 0;
+//
+//	counter++;
+//	sum += atoi(*(argv + 1));
+//
+//	(void) fprintf(stdout, "%i ", sum);
+//	if(counter == argcounter-1)
+//	{
+//		return;
+//	}
+//	prefix_sums(sum, (argv + 1));
+//}
 
-void prefix_sums(register int sum, char **argv)
-{
-	static int counter = 0;
+//void prefix_sums(register int size, char **argv)
 
-	counter++;
-	sum += atoi(*(argv + 1));
 
-	(void) fprintf(stdout, "%i ", sum);
-	if(counter == argcounter-1)
-	{
-		return;
-	}
-	prefix_sums(sum, (argv + 1));
-}
+ void prefix_sums(register int size, int n[])
+ {
+	 if(size < 2)
+	 {
+		 return;
+	 }
+	 int y[size/2];
+	 for(int i = 0; i < size/2; i++)
+	 {
+		 y[i] = n[2*i] + n[2*i + 1];
+	 }
+
+	 prefix_sums(size/2, y);
+
+	 n[1] = y[0];
+	 for(int i = 1; i < size/2; i++)
+	 {
+		 n[2*i] = y[i - 1] + n[2*i];
+		 n[2*i + 1] = y[i];
+	 }
+
+	 if((size%2) == 1)
+	 {
+		 n[size - 1] = y[size/2 - 1] + n[size - 1];
+	 }
+ }
 
 int main(int argc, char **argv)
 {
@@ -37,11 +67,24 @@ int main(int argc, char **argv)
 
 		return EXIT_FAILURE;
 	}
-	argcounter = argc;
+//	argcounter = argc;
 	//Following could be used for more than one run in one single program execution
 	//int input[argc - 1];
 
-	prefix_sums(0, argv);
+//	prefix_sums(0, argv);
 
+	int n[argc - 1];
+//	int y[argc - 1];
+	for(int i = 0; i < (argc - 1); i++)
+	{
+		n[i] = atoi(*(argv + i + 1));
+	}
+
+	prefix_sums((argc - 1), n);
+
+	for(int i = 0; i < (argc - 1); i++)
+	{
+		(void) fprintf(stdout, "%i ", n[i]);
+	}
 	return EXIT_SUCCESS;
 }
