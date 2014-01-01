a30c5f9ddd24dff39f394e44f8a7854c5ce01d37
adrian <adriankempf@mail.ua>
2014-01-01 21:25:32 +0100

    files für alle Projekte

---
+++ ps_hs_b_omp/ps_hs_b_omp.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ps_hs_b_omp.c
+ Author      : Adrian Kempf
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
